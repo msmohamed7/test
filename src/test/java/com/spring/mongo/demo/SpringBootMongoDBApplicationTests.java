e59f8fb37cc7d4696d63110bf599d9d57d5b9020
M S Mohamed <mohamed@Ms-MacBook-Pro.local>
2024-02-20 21:45:49 +0530

    git init

---
+++ src/test/java/com/spring/mongo/demo/SpringBootMongoDBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mongo.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootMongoDBApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
